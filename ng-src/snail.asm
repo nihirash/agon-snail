4a2678e28938cfbcd0f742b841a646e4e170b267
Alexander Sharihin <anihirash@gmail.com>
2024-07-24 00:20:46 +0200

    url parser/renderer

--- ng-src/snail.asm
+++ ng-src/snail.asm
@@ -3,39 +3,66 @@
 ;;
 ;; All rights are reserved
 
+DEV:    equ     1
+
     include "init.inc"
     include "mos.inc"
     include "vdp.inc"
     include "ui.inc"
+    include "top-menu.inc"
     include "agi-ram.inc"
     include "agi.inc"
-
+    include "gopher/page.inc"
+    include "url/parser.inc"
 _main:
     call vdp.init
 
+    ld hl, url.test
+    call url.parse
+    call url.render
+
+    .if DEV
+    call dev_version
+    .else
     call show_splash
+    .endif
 
-    ld hl, _main ;; NOT IMAGE :-)
-    call agi.show
-    
-    ld a, ui.msgbox.INFO
-    ld hl, test_msg
-    call ui.msgbox
-    MOSCALL mos_getkey
-
-    ld a, ui.msgbox.INPUT
-    ld hl, test_msg
-    call ui.msgbox
-    MOSCALL mos_getkey
-
-    ld a, ui.msgbox.DOWN
-    ld hl, test_msg
-    call ui.msgbox
-    MOSCALL mos_getkey
-
+    call ui.draw_header
+    call buffer.home_page
+    call gopher_page.init
 
     call vdp.free
     ret
 
-test_msg:
-    db "Hello, world!", 0
+.if DEV
+dev_version:
+    ld hl, @msg
+    ld a, ui.msgbox.INFO
+    call ui.msgbox
+    MOSCALL mos_getkey
+    ret
+@msg:
+    db "This is development version!", 0
+.endif
+
+process_function:
+    dl 0
+
+buffer.home_page:
+    ld hl, start_page
+    ld de, page_buffer
+    ld bc, start_page.end - start_page
+    ldir
+    ld (buffer_ends), de
+    ret
+
+start_page:
+    incbin "documents/test.gph"
+    db 0
+start_page.end:
+
+buffer_ends:
+    dl      page_buffer
+
+    include "url/buffers.inc"
+page_buffer:
